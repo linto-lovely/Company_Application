ec0a17b77252713c9a23a91a4741abc4d0e2cbce
linto-lovely <linznlinz@gmail.com>
2025-08-07 07:17:35 -0400

    Update SignUpPage.jsx

--- pages/SignUpPage.jsx
+++ pages/SignUpPage.jsx
@@ -12,7 +12,7 @@ const SignupPage = () => {
     setMessage('');
 
     try {
-      const res = await fetch('http://localhost:5001/auth/register', {
+      const res = await fetch('https://company-application.onrender.com/auth/register', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email, password })
